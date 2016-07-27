4a73e98be6b4b9dd8c1e4b964a19c9b1fa86d7d1
Daniel Buenrrostro <narutof1997@yahoo.com>
2016-07-26 23:11:58 -0500

    Initial commit

---
+++ kik/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let util = require('util');
+let http = require('http');
+let Bot  = require('@kikinteractive/kik');
+
+// Configure the bot API endpoint, details for your bot
+let bot = new Bot({
+    username: 'echo.bot',
+    apiKey: '259135bf-b2eb-4dfd-9d41-0198fede2d47',
+    baseUrl: 'https://github.com/iPwnPancakes/kik_memebot.git'
+});
+
+bot.updateBotConfiguration();
+
+bot.onTextMessage((message) => {
+    message.reply(message.body); // Reply with the text they sent
+});
+
+// Set up your server and start listening
+let server = http
+    .createServer(bot.incoming())
+    .listen(process.env.PORT || 8080);
